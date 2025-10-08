f63b688700a224022ecb16120372ec9c6298f46a
CHERM-MMXVIII <cherm4a@slsu.edu.ph>
2025-10-08 15:30:49 +0800

    server js

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
 
-// Serve static files (CSS, JS, images)
+// Serve static files
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/img', express.static(path.join(__dirname, 'img')));
 
-// Serve HTML files
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'index.html'));
+// Automatically serve HTML files
+const htmlFolder = path.join(__dirname, 'html');
+fs.readdirSync(htmlFolder).forEach(file => {
+  if (file.endsWith('.html')) {
+    const route = file === 'index.html' ? '/' : `/${file.replace('.html', '')}`;
+    app.get(route, (req, res) => {
+      res.sendFile(path.join(htmlFolder, file));
+    });
+  }
 });
 
-app.get('/services', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'services.html'));
-});
-
-// Catch all unknown routes (optional)
+// Catch-all 404
 app.get('*', (req, res) => {
   res.status(404).send('Page not found');
 });
